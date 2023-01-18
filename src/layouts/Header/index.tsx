65e2b59c02b0f1ea58848bf5bb57a42b768bf7bf
code-with-onye <onyemachitrust@gmail.com>
2023-01-18 00:26:49 -0800

    fix: link not clickable

--- src/layouts/Header/index.tsx
+++ src/layouts/Header/index.tsx
@@ -4,7 +4,7 @@ import { navLink } from "@/data";
 
 const Header = () => {
   return (
-    <div className="flex items-center w-full bg-gray-200 rounded-md bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10  p-4   drop-shadow-md fixed top-0 justify-between ">
+    <div className="flex items-center w-full bg-gray-200 rounded-md bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10  p-4   drop-shadow-md fixed top-0 justify-between z-50 ">
       <Logo />
       <div className="flex gap-x-4 text-xs font-semibold">
         {navLink.map((data) => (
